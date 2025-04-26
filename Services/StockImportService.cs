875cfbe61508f1fc539adf662842b1b3df920772
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-04-26 20:48:53 +0200

    Add files via upload

---
+++ Services/StockImportService.cs
@@ -0,0 +1,19 @@
+﻿using ProductStore.Application.DTOs;
+using ProductStore.Application.Interfaces;
+
+namespace ProductStore.Application.Services;
+
+public class StockImportService : IStockImportService
+{
+    private readonly IStockRepository _repository;
+
+    public StockImportService(IStockRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task ImportAsync(List<ImportProductDto> importProducts)
+    {
+        await _repository.ImportAsync(importProducts);
+    }
+}
