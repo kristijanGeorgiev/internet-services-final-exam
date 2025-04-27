f2e97c9d7908c580fc9b791500f2da8f07976fcc
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-04-27 13:48:23 +0200

    Delete Services directory

--- Services/IDiscountCalculatorService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Application.DTOs;
-
-namespace Core.Application.Services
-{
-    public interface IDiscountCalculatorService
-    {
-        Task<DiscountResultDto> CalculateDiscountAsync(IEnumerable<BasketItemDto> basket);
-    }
-}
