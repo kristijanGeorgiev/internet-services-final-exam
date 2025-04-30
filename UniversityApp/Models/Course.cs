e7c31d13a23a803852004ec6b8d1bcc79796bada
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-04-30 18:08:37 +0200

    Delete UniversityApp directory

--- UniversityApp/Models/Course.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Course
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public ICollection<Enrollment> Enrollments { get; set; }
-}
--- UniversityApp/Models/Student.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Student
-{
-    public int Id { get; set; }
-    public string FullName { get; set; }
-    public ICollection<Enrollment> Enrollments { get; set; }
-}
